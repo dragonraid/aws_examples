9032796709a23df39edaa65d4434ab78455f4d58
dragonraid <lucass.novotny@gmail.com>
2019-05-22 20:01:14 +0200

    delete foo.py

--- foo.py
+++
@@ -1,7 +0,0 @@
-import colorama
-from colorama import Fore, Back
-
-
-# Automatically adds a Style.RESET_ALL after each print statement
-print(Fore.RED + 'Red foreground text')
-print("Some plain text")
